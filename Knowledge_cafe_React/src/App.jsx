b212dacef10204254763ef7015061575613938ae
Mohammad-Abdur-Rahim <abdur.rahim2992001@gmail.com>
2024-04-06 16:13:50 +0600

    Header Complete

--- Knowledge_cafe_React/src/App.jsx
+++ Knowledge_cafe_React/src/App.jsx
@@ -1,5 +1,4 @@
 import './App.css'
-import Blogs from './Components/Blogs/Blogs'
 import Header from './Components/Header/Header'
 
 
@@ -9,9 +8,8 @@ function App() {
   return (
     <>
      
-      <h1 className='text-6xl text-start bg-red-300 '>Knowledge Cafe React Project Blog</h1>
    <Header></Header>
-   <Blogs></Blogs>
+ 
      
     </>
   )
--- Knowledge_cafe_React/src/Components/Header/Header.jsx
+++ Knowledge_cafe_React/src/Components/Header/Header.jsx
@@ -1,8 +1,9 @@
-
+import profile from '../../assets/profile1.png'
 const Header = () => {
     return (
-        <div>
-          <h2>Header section</h2>
+        <div className='flex justify-between p-10 items-center border-b-4'>
+          <h2 className="text-4xl font-bold">Knowledge Cafe</h2>
+          <img src={profile} alt="" />
             
         </div>
     );
