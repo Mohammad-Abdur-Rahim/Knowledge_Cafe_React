0a9fdcd7a65f693f265e486aa764a596b1519b9d
Mohammad-Abdur-Rahim <abdur.rahim2992001@gmail.com>
2024-04-06 17:19:58 +0600

    Blog Data Show and # tags map done

--- Knowledge_cafe_React/src/Components/Blog/Blog.jsx
+++ Knowledge_cafe_React/src/Components/Blog/Blog.jsx
@@ -1,9 +1,26 @@
 import PropTypes from 'prop-types';
 
 const Blog = ({blog}) => {
+    const {title,cover,author_img,reading_time,author,posted_date,hashtags}=blog;
     return (
         <div>
-            
+            <img className='w-full' src={cover} alt="" />
+            <div className='flex items-center justify-between'>
+                <div className='flex gap-5'>
+                    <img src={author_img} alt="" />
+                <div>
+                <h2 className='font-bold text-xl'>{author}</h2>
+                <p>{posted_date}</p>
+                </div>
+                </div>
+                <div>{reading_time} <span>Min Read</span></div>
+            </div>
+            <h2 className='text-4xl'>{title}</h2>
+            <p>
+        {
+            hashtags.map((hash,idx)=><span key={idx}><a href=''>#{hash}</a></span>)
+        }
+            </p>
         </div>
     );
 };
